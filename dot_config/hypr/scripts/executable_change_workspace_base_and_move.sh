73037b80bc63c8b72dd82ea3f1d66ee2a5e51b4b
pingzi <yuwenhao@stu.pku.edu.cn>
2025-11-29 21:18:54 +0800

    添加了工作区base地址操作，实现多个工作区环境切换

---
+++ dot_config/hypr/scripts/executable_change_workspace_base_and_move.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+if [ "$1" = "+" ]; then
+    tmp_base=$(cat /tmp/hyprland_workspace_base)
+    [[ "$tmp_base" -eq 90 ]] && echo 90 > /tmp/hyprland_workspace_base || echo $(($tmp_base + 10)) > /tmp/hyprland_workspace_base 
+elif [ "$1" = "-" ]; then 
+    tmp_base=$(cat /tmp/hyprland_workspace_base)
+    [[ "$tmp_base" -eq 0 ]] && echo 0 > /tmp/hyprland_workspace_base || echo $(($tmp_base - 10)) > /tmp/hyprland_workspace_base 
+else
+    echo $((($1 - 1) * 10)) > /tmp/hyprland_workspace_base
+fi
+hyprctl keyword animation workspaces,1,3,default,slidevert &&
+hyprctl dispatch workspace $(( $(cat /tmp/hyprland_workspace_base 2>/dev/null || echo 0) + $(($(hyprctl -j activeworkspace | jq -r .id) - 1)) % 10 + 1)) &&
+hyprctl keyword animation workspaces,1,3,default,slide
+
