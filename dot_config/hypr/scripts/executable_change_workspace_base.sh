1fd5b73f5e8a6bdb00ba73f415e234b0687cc4f2
pingzi <yuwenhao@stu.pku.edu.cn>
2025-12-01 06:28:22 +0800

    通过自己手动修改了waybar源代码来实现目标功能，并在PR被上游合并之前加入编译好的二进制waybar程序直接替代

--- dot_config/hypr/scripts/executable_change_workspace_base.sh
+++ dot_config/hypr/scripts/executable_change_workspace_base.sh
@@ -23,11 +23,11 @@ hyprctl keyword animation workspaces,1,3,default,slidevert &&
 # 获取当前base的所有workspace id 并重命名这样waybar可以不显示
 hyprctl -j workspaces | jq -r ".[] | \"\(.id) \(.name)\"" | while read id name; do
 if [[ "$(($old_base / 10 ))" = "$((($id - 1 ) / 10 ))" ]]; then
-        #hyprctl dispatch renameworkspace $id "n${name##n}"
-        hyprctl dispatch renameworkspace $id "$(($old_base / 10 + 1))${name##[0-9]}"
+        hyprctl dispatch renameworkspace $id "n${name##n}"
+        #hyprctl dispatch renameworkspace $id "$(($old_base / 10 + 1))${name##[0-9]}"
     elif [[ "$(($new_base / 10 ))" = "$((($id - 1 ) / 10 ))" ]]; then
-        #hyprctl dispatch renameworkspace $id "${name##n}"
-        hyprctl dispatch renameworkspace $id "${name##[0-9]}"
+        hyprctl dispatch renameworkspace $id "${name##n}"
+        #hyprctl dispatch renameworkspace $id "${name##[0-9]}"
     fi
 done
 hyprctl dispatch workspace $(( $(cat /tmp/hyprland_workspace_base 2>/dev/null || echo 0) + $(($(hyprctl -j activeworkspace | jq -r .id) - 1)) % 10 + 1)) &&
