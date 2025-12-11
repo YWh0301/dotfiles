c47686f60a6a4dede55ef81727a809c807c4df52
pingzi <yuwenhao@stu.pku.edu.cn>
2025-12-12 00:35:32 +0800

    添加mmk40键小键盘支持

--- dot_config/hypr/scripts/executable_hyprland_ipc_hook.sh
+++ dot_config/hypr/scripts/executable_hyprland_ipc_hook.sh
@@ -7,7 +7,7 @@ handle() {
         if [ -n "$window_address" ]; then
             xwayland=$(echo -n "j/clients" | socat - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket.sock | jq -r ".[] | select(.address == \"0x$window_address\") | .xwayland")
             if [ "$xwayland" = "true" ]; then
-                xkbcomp -w 0 ~/.config/custom.xkb $DISPLAY >/dev/null 2>&1
+                xkbcomp -w 0 ~/.config/xkb/custom.xkb $DISPLAY >/dev/null 2>&1
             fi
         fi ;;
   esac
